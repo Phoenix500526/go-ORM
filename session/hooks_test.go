d64e60dba3e55bdf72172d8497100e5ef23c1fdc
Phoenix <Phoenix500526@163.com>
2020-12-03 16:56:30 +0800

    通过反射获取结构体绑定的钩子，并支持在 CRUD 前后调用钩子

---
+++ session/hooks_test.go
@@ -0,0 +1,35 @@
+package session
+
+import (
+	"goorm/log"
+	"testing"
+)
+
+type Account struct {
+	ID       int `goorm:"PRIMARY KEY"`
+	Password string
+}
+
+func (account *Account) BeforeInsert(s *Session) error {
+	log.Info("before insert", account)
+	account.ID += 1000
+	return nil
+}
+
+func (account *Account) AfterQuery(s *Session) error {
+	log.Info("after query", account)
+	account.Password = "********"
+	return nil
+}
+
+func TestSession_CallMethod(t *testing.T) {
+	s := NewSession(TestDB, TestDial).Model(&Account{})
+	_ = s.DropTable()
+	_ = s.CreateTable()
+	_, _ = s.Insert(&Account{1, "123456"}, &Account{2, "1qazxcde3"})
+	u := &Account{}
+	err := s.First(u)
+	if err != nil || u.ID != 1001 || u.Password != "********" {
+		t.Fatal("Failed to call hooks after query, got ", u)
+	}
+}
